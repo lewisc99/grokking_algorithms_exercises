17391dabd85cda7e81a479169e17e6dabf530572
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-07-06 14:52:50 -0300

    21_greedy_algorithm_fruits.py

---
+++ 21_greedy_algorithm_fruits.py
@@ -0,0 +1,7 @@
+fruits = set(["avocado", "tomato", "banana"])
+vegetables = set(["beets", "carrots", "tomato"])
+print("fruits | vegetables : ", fruits | vegetables)
+print("fruits & vegetables : ", fruits & vegetables)
+print("fruits - vegetables : ", fruits - vegetables)
+print("vegetables - fruits : ", vegetables- fruits)
+
